8483b899fb70c2dca10efb3ff0b37196dc6d5a54
wangyichen1064431086 <1064431086@qq.com>
2017-06-25 15:56:31 +0800

    Study for the usage of o-video

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,12 @@
 import Header from 'o-header';
 
 Header.init();
+
+import OVideo from 'o-video';
+const opts = {
+    id: 4165329773001,
+	optimumwidth: 710,
+	placeholder: true,
+	classes: ['video']
+};
+const video = new OVideo(document.body,opts);
